4e66c93929d96ba297e9f2a2ca5928237daec1d2
Arnaud <bandonkeyea@gmail.com>
2022-06-10 00:55:26 -0600

    Create components and link them

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <header className="top_header">
+      <div className="left_side">
+        <h1>Book Store</h1>
+      </div>
+      <div className="right_side">
+        <Link className="menu_bar" to="/">Books</Link>
+        <Link className="menu_bar" to="/Categories">Categories</Link>
+      </div>
+      <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/Categories" element={<Categories />} />
+      </Routes>
+
+    </header>
+    
   );
 }
 
---
+++ src/components/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <button>Check updates</button>
+    )
+}
+
+export default Categories;
