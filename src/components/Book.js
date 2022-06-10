ee213bdff21c4aadad3b6ae1a23ec57580164df5
Arnaud <bandonkeyea@gmail.com>
2022-06-10 04:29:12 -0600

    Correct the topic in book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = (props) => {
-  const { topics, title, author } = props;
+  const { topic, title, author } = props;
   return (
     <div className="container">
         <div className="card">
-            <div>{topics}</div>
+            <div>{topic}</div>
             <div>{title}</div>
             <div>{author}</div>
         </div>
