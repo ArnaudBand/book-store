105fb8470c4b4f243c62868955b5a4512eadc8ea
Arnaud <bandonkeyea@gmail.com>
2022-06-10 02:37:44 -0600

    Display books

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
 function App() {
   return (
-    <header className="top_header">
-      <div className="left_side">
-        <h1>Book Store</h1>
-      </div>
-      <div className="right_side">
-        <Link className="menu_bar" to="/">Books</Link>
-        <Link className="menu_bar" to="/Categories">Categories</Link>
-      </div>
+    <>
+      <header className="top_header flex">
+        <div className="left_side flex">
+          <h1 className="logo">BookStore</h1>
+          <Link className="menu_bar" to="/">Books</Link>
+          <Link className="menu_bar" to="/Categories">Categories</Link>
+        </div>
+        <div className="right_side">
+          <FaUser />
+        </div>
+      </header>
       <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/Categories" element={<Categories />} />
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
       </Routes>
-
-    </header>
+  </>
     
   );
 }
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
 const Books = () => {
+  const booksArray = [
+    {
+      id: 1,
+      topic: 'Action',
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: 2,
+      topic: 'Science Fiction',
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: 3,
+      topic: 'Economy',
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ]
+
     return (
-        <h1>Hello</h1>
-    )
+      <div>
+        <h2>Books</h2>
+        {booksArray.map((book) => (
+          <div key={book.id}>
+            <span>{book.topic}</span>
+            <span>{book.title}</span>
+            <span>{book.author}</span>
+          </div>
+        ))}
+      </div>
+    );
 }
 
 export default Books;
