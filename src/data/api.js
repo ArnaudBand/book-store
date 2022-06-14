c95cba1f42f12c5ebd34858baf9a45bbc12462af
Arnaud <bandonkeyea@gmail.com>
2022-06-14 14:51:49 -0600

    create data folder for api

---
+++ src/data/api.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/4uMXQcftqx4Vw0tVETqM/books/';
+export const HEADERS = {
+  'content-type': 'application/json; charset=UTF-8',
+};
